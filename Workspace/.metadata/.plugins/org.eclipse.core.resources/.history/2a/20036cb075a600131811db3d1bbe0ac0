1b278d517984facfe5066516a799de44493de439
Andrew Shepherd <shepherd_a@hotmail.com>
2014-03-08 15:04:54 +1100

    Wired up the Edit activity to the main activity

---
+++ Workspace/.metadata/.plugins/org.eclipse.core.resources/.history/2a/20036cb075a600131811db3d1bbe0ac0
@@ -0,0 +1,16 @@
+package com.example.taskreminder;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ReminderEditActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		
+	}
+
+	
+}
--- Workspace/TaskReminder/src/com/example/taskreminder/ReminderEditActivity.java
+++ Workspace/TaskReminder/src/com/example/taskreminder/ReminderEditActivity.java
@@ -2,13 +2,32 @@ package com.example.taskreminder;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class ReminderEditActivity extends Activity {
 
+	
+	private Button getConfirmButton() {
+		return (Button)this.findViewById(R.id.confirm);
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		super.setContentView(R.layout.reminder_edit);
+		
+		this.getConfirmButton().setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				ReminderEditActivity.this.finish();
+				
+			}
+			
+		});
 		
 	}
 
